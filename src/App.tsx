d37a1c33219b26c365604cd7b75962413613d619
sudhirdontha72 <sudhirdontha@outlook.com>
2021-01-24 01:04:57 +0530

    Added imports for style, Added App.style.ts file
    Added imports under style,
    Added App.style.ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import {useQuery} from 'react-Query';
+
 //components required - material-ui
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
@@ -7,6 +8,8 @@ import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 
+//style
+import {Wrapper} from './App.styles';
 
 const App = () => {
   return (
